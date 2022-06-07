c14c1bd3f271a4927a13c83bcfa68f6b64100c72
myigitaktas <myigit13@outlook.com>
2022-06-07 22:41:47 +0300

    Camera fix kinda (still doesnt work)

---
+++ ISO RPG 2021/Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 4, 0);
+    }
+}
