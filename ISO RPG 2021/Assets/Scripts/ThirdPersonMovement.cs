f73711ba1ecf796b088fcb558be1c30f053dda23
ipekk <ipekog@outlook.com>
2022-06-08 15:09:18 +0300

    Fixed Camera + Movement + Animation of Protagonist
    - organize folders (assets, scripts, materials, etc.)
    - animation (remove jump mechanic)
    - added new scripts to fix the movement/camera

---
+++ ISO RPG 2021/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+
+    public Animator anim;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f)
+;
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        if (direction != Vector3.zero)
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+}
