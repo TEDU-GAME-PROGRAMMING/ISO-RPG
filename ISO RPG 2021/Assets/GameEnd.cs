49e4ca09f3334afddd918032eeaa97c0a7ff2e52
ipekk <ipekog@outlook.com>
2022-06-09 16:56:33 +0300

    Enfing Dying Menu Game Screens adjusted

---
+++ ISO RPG 2021/Assets/GameEnd.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+    //Destroy(gameobj, 2f);
+    //SceneManager.LoadScene("Ending");
+
+
+
+
+}
